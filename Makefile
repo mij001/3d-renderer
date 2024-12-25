8e8d8b7788f388783459d64ddad791ffb712d957
Inomal <no@yolo.lol>
2024-12-26 03:00:11 +0530

    get rect

---
+++ Makefile
@@ -0,0 +1,2 @@
+build:
+	gcc -Wall -Wextra -std=c99 ./src/*.c -o renderer
---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+int main(void)
+{
+    printf("hi mom!  \n");
+    return 0;
+}
